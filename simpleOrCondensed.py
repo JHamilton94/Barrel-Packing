e8c5c19555cc980ba3ccb15d2776edc7d584839e
JHamilton94 <hamilton9455@gmail.com>
2022-04-02 16:40:31 -0300

    Moved algorithm to its own package, added command line argument functionality

--- simpleOrCondensed.py
+++ simpleOrCondensed.py
@@ -1,55 +1,13 @@
-import math
+#!/usr/bin/env python
+import packing
+import sys
 
-#Problem Parameters
-HOLDWIDTH = 102.0
-HOLDHEIGHT = 3.0
+BARRELRADIUS = float(sys.argv[1])
+HOLDWIDTH = float(sys.argv[2])
+HOLDHEIGHT = float(sys.argv[3])
 
-BARRELRADIUS = 1.0
+barrelTotalSP = packing.simplePacking(BARRELRADIUS, HOLDWIDTH, HOLDHEIGHT)
+barrelTotalCP = packing.condensedPacking(BARRELRADIUS, HOLDWIDTH, HOLDHEIGHT)
 
-#Calculated Parameters
-barrelDiameter = BARRELRADIUS*2
-
-#Simple Packing
-horizontalBarrels = int(HOLDWIDTH/barrelDiameter)
-verticalBarrels = int(HOLDHEIGHT/barrelDiameter)
-barrelTotalSP = horizontalBarrels*verticalBarrels
-
-print("With simple packing the hold can carry {a} barrels.".format(a=barrelTotalSP))
-
-#Condensed Packing
-oddRowBarrelNum = int(HOLDWIDTH/barrelDiameter)
-oddRowBarrelRemainder = (HOLDWIDTH/barrelDiameter)-int(HOLDWIDTH/barrelDiameter)
-
-evenRowBarrelNum = 0
-
-print(101.0/2)
-print("Barrels before cast: {a}".format(a=HOLDWIDTH/barrelDiameter))
-print("Barrels after cast: {a}".format(a=int(HOLDWIDTH/barrelDiameter)))
-print("Barrel remainder: {a}".format(a=oddRowBarrelRemainder))
-
-if oddRowBarrelRemainder >= 0.5:
-  evenRowBarrelNum = oddRowBarrelNum
-  print("EXTRA BARREL!")
-else:
-  evenRowBarrelNum = oddRowBarrelNum - 1
-  print("No extra barrel :(")
-
-doubleRowHeight = ((2+math.sqrt(3))*BARRELRADIUS)
-
-
-totalDoubleRows = int(HOLDHEIGHT/doubleRowHeight)
-
-evenRowAdditionalHeight = doubleRowHeight-barrelDiameter
-
-barrelTotalCP = totalDoubleRows*(evenRowBarrelNum + oddRowBarrelNum)
-
-print("Odd row num: {a}, even row num: {b}".format(a=oddRowBarrelNum, b=evenRowBarrelNum))
-print("TotalDoubleRows: {a}".format(a=totalDoubleRows))
-print("BarrelTotal before extra row: {a}".format(a=barrelTotalCP))
-
-#Check if room for an additional even row
-if HOLDHEIGHT-(totalDoubleRows*doubleRowHeight) >= evenRowAdditionalHeight:
-  barrelTotalCP+=evenRowBarrelNum
-  
-print("With condensed packing the hold can carry {a} barrels.".format(a=barrelTotalCP))
-
+print("With simple packing the hold can carry {a} barrels".format(a=barrelTotalSP))
+print("With condensed packing hte hold can carry {a} barrels.".format(a=barrelTotalCP))
